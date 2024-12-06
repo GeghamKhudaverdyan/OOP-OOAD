407e492ca7d7c8119bcb1d24dc1c794a076f507e
GeghamKhudaverdyan <geghamkhudaverdyan686@gmail.com>
2024-12-06 14:58:07 +0400

    Delete coffeeShop.cpp directory

--- coffeeShop.cpp/src/coffee.cpp
+++
@@ -1,9 +0,0 @@
-#include "../hpp/coffee.hpp"
-
-std::string Coffee::getDescription() const  {
-    return "Coffee ";
-}
-
-double Coffee::cost() const {
-    return m_cost;
-}
--- coffeeShop.cpp/src/tea.cpp
+++
@@ -1,9 +0,0 @@
-#include "../hpp/tea.hpp"
-
-std::string Tea::getDescription() const {
-    return "Tea ";
-}
-
-double Tea::cost() const {
-    return m_cost;
-}
